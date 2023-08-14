1d344d381d3fff3114b6160dbd9f5f57a884292d
Husseljo <husseljoo@gmail.com>
2023-08-14 23:34:53 +0300

    nvim run_file mappings formatted using loops

--- nvim/lua/run_file_mappings.lua
+++ nvim/lua/run_file_mappings.lua
@@ -1,26 +1,48 @@
 local create_augroup = require("utils").create_augroup
 
-local skeletons = {
-	{ "BufNewFile", "*.py", "0r ~/.vim/templates/skeleton.py" },
-	{ "BufNewFile", "*.go", "0r ~/.vim/templates/skeleton.go" },
-	{ "BufNewFile", "*.dart", "0r ~/.vim/templates/skeleton.dart" },
-	{ "BufNewFile", "*.html", "0r ~/.vim/templates/skeleton.html" },
-	-- { "BufNewFile", "/home/husseljo/interview_preparation/*.py", "0r ~/.vim/templates/leetcode_skeleton.py" },
+local run_commands = {
+	{ "python", "python %" },
+	{ "go", "go run %" },
+	{ "rust", "cargo run" },
+	{ "dart", "dart run %" },
+	{ "cpp", "g++ % -o %:r && ./%:r<CR>:!rm %:r" },
 }
+local skeleton_langs = { "py", "dart", "html", "go", "cpp" }
+local base = "0r ~/.vim/templates/skeleton."
+
+local custom = {
+	{ "FileType", "cpp", "setlocal shiftwidth=2 tabstop=2" },
+}
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+
+local function generate_skeletons()
+	local res = {}
+	for _, lang in ipairs(skeleton_langs) do
+		local x = { "BufNewFile", "*." .. lang, base .. lang }
+		table.insert(res, x)
+	end
+	return res
+end
+
+local function run_files()
+	local res = {}
+	for _, val in ipairs(run_commands) do
+		local x = { "FileType", val[1], "nnoremap <buffer> <F9> :wa<CR> :!" .. val[2] .. "<CR>" }
+		table.insert(res, x)
+	end
+	return res
+end
+
+local skeletons = generate_skeletons()
+local running_files = run_files()
+
 local printing = {
 	{ "FileType", "python", 'vnoremap <leader>p <Esc>0wyiwoprint(f": {}")<Esc>0f:Pf{p' },
 	{ "FileType", "python", 'nmap <leader>p <Esc>0wyiwoprint(f": {}")<Esc>0f:Pf{p' },
 	{ "FileType", "rust", "nmap <buffer> \\p oprintln!();<Esc>$F)i" },
 	{ "FileType", "rust", "nmap <buffer> \\P Oprintln!();<Esc>$F)i" },
-}
-local running_files = {
-	{ "FileType", "python", "nnoremap <buffer> <F9> :wa<CR> :!python %<CR>" },
-	{ "FileType", "go", "nnoremap <buffer> <F9> :wa<CR> :!go run %<CR>" },
-	{ "FileType", "dart", "nnoremap <buffer> <F9> :wa<CR> :!dart run %<CR>" },
-	{ "FileType", "rust", "nnoremap <buffer> <F9> :wa<CR> :!cargo run<CR>" },
-}
-local custom = {
-	{ "FileType", "cpp", "setlocal shiftwidth=2 tabstop=2" },
 }
 
 create_augroup(skeletons, "skeletons")
