5e8b0802a21d8be1c2f0aec7fdb29dbdac0b679f
Husseljo <husseljoo@gmail.com>
2024-01-10 21:59:23 +0200

    fix terminal mappings later

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -90,10 +90,21 @@ map("n", "<leader>g", ":LazyGit<CR>")
 map("n", "<leader>n", ":NvimTreeToggle<CR>")
 
 -- terminal mode mappings
-map("t", "<C-j>", "<C-\\><C-n>") -- regular escape
-map("t", "<leader><Tab>", "<C-\\><C-n><C-w>w")
-map("t", "<C-w>", "<C-\\><C-n><C-w>")
-vim.cmd("autocmd BufWinEnter,WinEnter,BufEnter term://* startinsert")
+-- map("t", "<C-w>", "<C-\\><C-n><C-w>") --could be handy for unification, however I rely on C-w word delete too much
+-- map("t", "<C-j>", "<C-\\><C-n>") -- regular escape
+-- map("t", "<C-k>", "<C-\\><C-n>") -- regular escape
+-- map("t", "<Esc>", "<C-\\><C-n><C-w>w")
+-- vim.cmd("autocmd BufWinEnter,WinEnter,BufEnter term://*[^lazygit] startinsert")
+
+-- vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter" }, {
+	pattern = { "*toggleterm*" },
+	callback = function()
+		vim.cmd("startinsert")
+		map("t", "<Esc>", "<C-\\><C-n>") -- regular escape
+		-- map("t", "<C-j>", "<C-\\><C-n><C-w>w") -- leave terminal
+	end,
+})
 
 -- toggle terminal
 map("n", "<leader>t", ":ToggleTerm direction=float<CR>")
