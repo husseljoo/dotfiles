2e4872b07effa9884cf4a64a845c4cdecb5e09a6
Husseljo <husseljoo@gmail.com>
2024-01-20 00:02:32 +0200

    add terminal to seperate file

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -90,27 +90,6 @@ map("n", "<leader>g", ":LazyGit<CR>")
 -- toggle nvim-tree
 map("n", "<leader>n", ":NvimTreeToggle<CR>")
 
--- terminal mode mappings
--- map("t", "<C-w>", "<C-\\><C-n><C-w>") --could be handy for unification, however I rely on C-w word delete too much
--- map("t", "<C-j>", "<C-\\><C-n>") -- regular escape
--- map("t", "<C-k>", "<C-\\><C-n>") -- regular escape
--- map("t", "<Esc>", "<C-\\><C-n><C-w>w")
--- vim.cmd("autocmd BufWinEnter,WinEnter,BufEnter term://*[^lazygit] startinsert")
-
--- vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter" }, {
-	pattern = { "*toggleterm*" },
-	callback = function()
-		vim.cmd("startinsert")
-		map("t", "<Esc>", "<C-\\><C-n>") -- regular escape
-		-- map("t", "<C-j>", "<C-\\><C-n><C-w>w") -- leave terminal
-	end,
-})
-
--- toggle terminal
-map("n", "<leader>t", ":ToggleTerm direction=float<CR>")
-map("n", "<C-t>", ":ToggleTerm size=17<CR>")
-
 -- write qq to start recording
 map("n", "<F5>", "@q") -- replay macro in register q
 map("v", "<F5>", ":norm @q<CR>")
---
+++ nvim/lua/terminal.lua
@@ -0,0 +1,20 @@
+-- terminal mode mappings
+-- map("t", "<C-w>", "<C-\\><C-n><C-w>") --could be handy for unification, however I rely on C-w word delete too much
+-- map("t", "<C-j>", "<C-\\><C-n>") -- regular escape
+-- map("t", "<C-k>", "<C-\\><C-n>") -- regular escape
+-- map("t", "<Esc>", "<C-\\><C-n><C-w>w")
+-- vim.cmd("autocmd BufWinEnter,WinEnter,BufEnter term://*[^lazygit] startinsert")
+
+-- vim.api.nvim_create_autocmd({ "TermOpen", "TermEnter" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter" }, {
+	pattern = { "*toggleterm*" },
+	callback = function()
+		vim.cmd("startinsert")
+		map("t", "<Esc>", "<C-\\><C-n>") -- regular escape
+		-- map("t", "<C-j>", "<C-\\><C-n><C-w>w") -- leave terminal
+	end,
+})
+
+-- toggle terminal
+map("n", "<leader>t", ":ToggleTerm direction=float<CR>")
+map("n", "<C-t>", ":ToggleTerm size=17<CR>")
