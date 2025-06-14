c66eb5e8964016f9070feba4d9a26049a5d97860
Husseljo <husseljoo@gmail.com>
2025-06-14 19:41:50 +0300

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
+vim.g.neovim_mode = vim.env.NEOVIM_MODE or "default"
+
 require("core.lsp")
 require("core.lazy")
 -- require("plugins_loader")
@@ -6,6 +8,7 @@ require("core.lazy")
 -- require("lsp-zero-config")
 require("config.base")
 require("config.mappings")
+require("config.autocmds")
 -- require("terminal")
 require("config.run_file_mappings")
 require("config.colorscheme")
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,35 +1,20 @@
 return {
   "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  --Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations
-  lazy = false,
-  skip_confirm_for_simple_edits = true,
-  -- prompt_save_on_select_new_entry = false,
-  view_options = {
-    -- show_hidden = true,
-  },
-  keymaps = {
-    ["<C-h>"] = { "actions.parent", mode = "n" },
-    -- ["g?"] = { "actions.show_help", mode = "n" },
-    -- ["<CR>"] = "actions.select",
-    -- ["<C-s>"] = { "actions.select", opts = { vertical = true } },
-    -- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
-    -- ["<C-t>"] = { "actions.select", opts = { tab = true } },
-    -- ["<C-p>"] = "actions.preview",
-    -- ["<C-c>"] = { "actions.close", mode = "n" },
-    -- ["<C-l>"] = "actions.refresh",
-    -- ["-"] = { "actions.parent", mode = "n" },
-    -- ["_"] = { "actions.open_cwd", mode = "n" },
-    -- ["`"] = { "actions.cd", mode = "n" },
-    -- ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
-    -- ["gs"] = { "actions.change_sort", mode = "n" },
-    -- ["gx"] = "actions.open_external",
-    -- ["g."] = { "actions.toggle_hidden", mode = "n" },
-    -- ["g\\"] = { "actions.toggle_trash", mode = "n" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    columns = {
+      "icon",
+      -- "size",
+    },
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+    },
+    keymaps = {
+      ["<Esc>"] = { "actions.close", mode = "n" },
+      -- ["q"] = { "actions.close", mode = "n" }, -- intuitive, but maybe I would want to do a macro
+      ["<C-h>"] = { "actions.parent", mode = "n" },
+      ["<C-l>"] = { "actions.select", mode = "n" },
+    },
   },
 }
